ec739f08f46e632c63566b3a6247c249712028ce
Simone <simich95@hotmail.it>
2026-03-27 15:35:27 +0100

    Second commit, create first neural network and train it

--- main_project.py
+++ main_project.py
@@ -8,3 +8,34 @@ y = torch.tensor([13, 18, 24, 12, 20, 26, 12, 18, 24, 14, 21, 28, 12, 20], dtype
 
 plt.scatter(x,y)
 
+# =============================================================================================================================
+# Let's create our first simple neural network with a single neuron
+
+torch.manual_seed(42)
+model = torch.nn.Linear(1,1) # Calculate a simple linear function using weights and bias
+                             # Changing W modifies the slope of the line; changing B modifies its position
+                             # The numbers represent the number of inputs and outputs; the number of outputs corresponds to the number of neurons
+loss = torch.nn.MSELoss()    # I create the loss function to calculate the error; for a regression problem, I use MSE (Mean Squared Error)
+optimizer = torch.optim.Adam(model.parameters(), lr = 0.1) # After calculating the loss function, we calculate the gradients of each parameter; the larger the gradient, the greater the parameter's impact on the error
+model(x)
+
+# =============================================================================================================================
+# In this step, we'll train our model
+
+loss = torch.nn.MSELoss() # Instantiate the MSELoss class
+for epoch in range(3000): # Let's start with 1000, then we'll switch to more epochs because the error was still decreasing
+  optimizer.zero_grad()   # Set all gradients to zero
+  y_pred = model(x)
+  loss_fn = loss(y_pred, y)
+  loss_fn.backward()
+  optimizer.step()
+  if epoch % 100 == 0:
+    print(f"Epoch: {epoch} Loss: {loss_fn}")
+  if epoch in [0, 2, 4, 15, 2999]:
+    plt.plot(x, y_pred.detach(), label = f"Epoch: {epoch}")
+
+plt.scatter(x,y)
+plt.legend()
+
+# =============================================================================================================================
+
